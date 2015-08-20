f18182a6d137f9da23c09522cf8e228b6bdbcf7e
igusaurus <igusaurus@mac.com>
2015-08-20 04:01:43 +0200

    HTTPS

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -38,7 +38,7 @@ exports.create = function(req, res) {
 	var login    = req.body.login;
 	var password = req.body.password;
 
-	var userController = require('./user_Controller');
+	var userController = require('user_Controller');
 	userController.autenticar(login, password, function (error, user) {
 		if (error) { // retornamos los errores de session
 			req.session.errors = [{"message": "Se ha producido un error:" + error}];
